dfe01dfd12e7f4a06fbd2ebf5cd71601a74fdd6b
Praveen kumar <praveenkumarpvnk@gmail.com>
2026-01-23 17:16:40 +0530

    show only the G shift code and change the footer name

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -5,7 +5,7 @@ const Footer = () => (
     <Row className="justify-content-center text-center fs--1 mt-4 mb-3">
       <Col sm="auto">
         <p className="mb-0 text-600 text-center">
-          Vipras Facility Management Solutions Pvt Ltd., All rights reserved.{' '}
+          Vipras Plus All rights reserved.{' '}
           <span className="d-none d-sm-inline-block">| </span>
           <br className="d-sm-none" /> {new Date().getFullYear()} &copy;{' '}
         </p>
